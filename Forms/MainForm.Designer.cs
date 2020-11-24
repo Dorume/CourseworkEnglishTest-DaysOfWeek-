2b30cac814ec1561358992e081f6b0737dc7528b
Савицкий Даня <savitskiydanulo@gmail.com>
2020-11-24 17:27:46 +0200

    Создал структуру проекта.

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Lets__study_
+﻿namespace Forms.Lets__study_
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 411);
+            this.Name = "MainForm";
+            this.Text = "Let\'s study!";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Forms/QuestionFrm.cs
+++ Forms/QuestionFrm.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Lets__study_
+namespace Lets__study_.Forms
 {
-    public partial class Form1 : Form
+    public partial class QuestionFrm : Form
     {
-        public Form1()
+        public QuestionFrm()
         {
             InitializeComponent();
         }

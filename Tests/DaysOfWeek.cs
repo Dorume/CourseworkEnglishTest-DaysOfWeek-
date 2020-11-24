d36863d4e286e4e7594c1f895ed0ea4a28dedca5
Савицкий Даня <savitskiydanulo@gmail.com>
2020-11-24 19:44:17 +0200

    Создал вопросы.

---
+++ Tests/DaysOfWeek.cs
@@ -0,0 +1,20 @@
+﻿using Lets__study_.Tests.Interface;
+using Lets__study_.Tests.Questions;
+using System.Collections.Generic;
+
+namespace Lets__study_.Tests
+{
+    class DaysOfWeek : ITest
+    {
+        public IList<QuestionBase> Questions { get; set; } = new List<QuestionBase>()
+        {
+            new QuestionBase { Question = "Monday", Answer = "Понеділок"},
+            new QuestionBase { Question = "Tuesday", Answer = "Вівторк"},
+            new QuestionBase { Question = "Wednesday", Answer = "Середа"},
+            new QuestionBase { Question = "Thursday", Answer = "Четверг"},
+            new QuestionBase { Question = "Friday", Answer = "Пятниця"},
+            new QuestionBase { Question = "Saturday", Answer = "Субота"},
+            new QuestionBase { Question = "Sunday", Answer = "Неділя"}
+        };
+    }
+}
---
+++ Tests/Questions/QuestionBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Lets__study_.Tests.Questions
+{
+    class QuestionBase
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
